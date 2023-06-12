2bd3ebd3a28d074002f3bd765aee62a72a623ab2
BRUNO AQUILES DE LIMA <bal22@inf.ufpr.br>
2023-06-12 17:33:41 -0300

    Organização

--- src/vina.c
+++ src/vina.c
@@ -5,34 +5,34 @@
 
 struct membro {
     char *nome;
-    unsigned long comecoDados;
+    unsigned long comeco_dados;
     unsigned long posicao;
     uid_t uid;
     off_t tam;
     mode_t permissoes;
-    struct timespec ultimaMod;
+    struct timespec ult_mod;
 };
 
 struct diretorio {
     FILE *archive;
     unsigned long tam;
-    struct membro *mbrs;
+    struct membro **mbrs;
 };
 
-void faltaDeMemoria()
+void falta_de_memoria()
 {
     fprintf(stderr, "Erro: Falta de memoria.\n");
     exit(1);
 }
 
-void criaNovoDir(struct diretorio *dir, char *arc)
+void cria_novo_dir(struct diretorio *dir, char *arc)
 {
     dir->archive = fopen(arc, "w");
     dir->mbrs = NULL;
     dir->tam = 0;
 }
 
-void lerArchive(struct diretorio *dir)
+void ler_archive(struct diretorio *dir)
 {
     unsigned long posiDir;
     fscanf(dir->archive, "%ld", &posiDir);
@@ -40,26 +40,35 @@ void lerArchive(struct diretorio *dir)
     fscanf(dir->archive, "%ld", &dir->tam);
     dir->mbrs = malloc(sizeof(struct membro*) * dir->tam);
 
-    for (unsigned long i = 0; i < dir->tam; i++) {
-        fread(dir->mbrs + i, sizeof(struct membro), 1, dir->archive);
-    }
+    for (unsigned long i = 0; i < dir->tam; i++)
+        fread(dir->mbrs[i], sizeof(struct membro), 1, dir->archive);
 }
 
-int iniciaDiretorio(char *archive)
+int inicia_diretorio(char *archive)
 {
     struct diretorio *dir = malloc(sizeof(struct diretorio));
     if (dir == NULL)
-        faltaDeMemoria();
+        falta_de_memoria();
     dir->archive = fopen(archive, "r+");
     if (dir->archive == NULL)
-        criaNovoDir(dir, archive);
+        cria_novo_dir(dir, archive);
     else
-        lerArchive(dir);
+        ler_archive(dir);
 
     return 0;
 }
 
-int adicionaMembro(char *nome, struct diretorio *dir)
+void pega_props(char *nome, struct membro *mem, struct stat props, unsigned long tam)
+{
+    mem->nome = nome;
+    mem->comeco_dados = tam;
+    mem->uid = props.st_uid;
+    mem->tam = props.st_size;
+    mem->permissoes = props.st_mode;
+    mem->ult_mod = props.st_mtim;
+}
+
+int add_membro(char *nome, struct diretorio *dir)
 {
     struct stat propriedades;
     int ok = stat(nome, &propriedades);
@@ -67,20 +76,18 @@ int adicionaMembro(char *nome, struct diretorio *dir)
         return 1;
     struct membro *mem = malloc(sizeof(struct membro));
     if (mem == NULL)
-        faltaDeMemoria();
-    mem->nome = nome;
-    mem->comecoDados = ftell(dir->archive);
-    mem->uid = propriedades.st_uid;
-    mem->tam = propriedades.st_size;
-    mem->permissoes = propriedades.st_mode;
-    mem->ultimaMod = propriedades.st_mtim;
+        falta_de_memoria();
+
+    pega_props(nome, mem, propriedades, ftell(dir->archive));
+    
+    dir->mbrs[dir->tam] = mem;
 
     return 0;
 }
 
 int main()
 {
-    /* adicionaMembro("makefile"); */
+    /* add_membro("makefile"); */
 
     return 0;
 }
