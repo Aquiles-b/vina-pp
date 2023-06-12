6c0ab4415ecf9c7c919b0cbdc7812b4d25298bb3
BRUNO AQUILES DE LIMA <bal22@inf.ufpr.br>
2023-06-12 14:35:22 -0300

    struct membro

--- src/vina.c
+++ src/vina.c
@@ -1,9 +1,19 @@
 #include <stdio.h>
+#include <sys/stat.h>
+#include <time.h>
+
+struct membro {
+    char *nome;
+    unsigned long comecoDados;
+    unsigned long posicao;
+    uid_t uid;
+    off_t tam;
+    mode_t permissoes;
+    struct timespec ultimaMod;
+};
 
 int main()
 {
-    printf ("Aooo\n");
-    printf ("Aooo\n");
 
     return 0;
 }
